99fe1ab5183e11544ec86911867d7fb8e9e2e22e
Adhilhabeeb <adhilhabeb960571@gmail.com>
2026-03-10 11:34:33 +0530

    add

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,8 @@
 import express from "express";
 import cors from "cors";
 import { publishToVercel } from "./publish.js";
-
+import dotenv from "dotenv";
+dotenv.config()
 const app = express();
 
 app.use(cors()); // ✅ allow all origins
